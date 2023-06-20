f5ba9e87404696d438bd7dc77670eecdf7ff391c
Valiullin Bulat <79865289+offme16@users.noreply.github.com>
2023-06-20 18:06:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".reviews-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
